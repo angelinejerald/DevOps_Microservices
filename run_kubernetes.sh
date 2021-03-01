e6fcdced02827f5897a3d4db1c7bf0068a1a5790
EC2 Default User <ec2-user@ip-172-31-19-116.us-east-2.compute.internal>
2021-03-01 10:37:30 +0000

    Operationalize-a-Machine-Learning-Microservice-API

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="angeline95/mluapi"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlumicroserviceapi\
+	    --generator=run-pod/v1\
+	        --image=$dockerpath\
+		    --port=80 --labels app=mlumicroserviceapi
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlumicroserviceapi 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+ dockerpath=angeline95/mluapi
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u angeline95 &&\
+	    docker image tag mluapi $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
